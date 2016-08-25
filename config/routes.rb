b1fa80f2884e5d495425d443d3f382d1abd76934
三吉愛 <ai_miyoshi@miyoshiai-no-MacBook-Pro.local>
2016-08-25 18:58:34 +0900

    create controller&view&database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'reviews/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
