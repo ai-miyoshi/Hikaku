70ce06c1da07eabbffdcf1d6cd4fdfd0698bed66
三吉愛 <ai_miyoshi@miyoshiai-no-MacBook-Pro.local>
2016-08-26 15:31:24 +0900

    edit scraping file

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,43 +1,44 @@
 class Scraping
+    require 'mechanize'
 
-  def self.product_urls
-
-  end
-
-  def self.product_name
-    
-  end
-
-  def self.image_url
-
-    def scraping_image(links)
+    def self.product_info
+      links = []      # 個別ページのリンクを保存する配列
+      # 取得ぺージの指定
       agent = Mechanize.new
       agent.user_agent_alias = 'Windows IE 7'
-      # 取得ぺージの指定(認証画面にまず飛ぶ)
-      page = agent.get(links)
-      next_page = page.link_with(:text => 'は　い').click      # 認証画面で「はい」をクリック
-      current_page = agent.get(links)     # リンク取得ぺージの指定
-      image_url = current_page.at('dt img').get_attribute('src')     # リンクの取得元を指定
-      return 'http://www.gcdental.co.jp' + image_url
+      # 認証画面に飛ぶので，認証画面で「はい」をクリック
+      url = 'http://www.gcdental.co.jp/sys/data/category/20/'
+      page = agent.get(url)
+      next_page = page.link_with(:text => 'は　い').click
+      # 再度取得ぺージの指定と個別ページのリンクの取得
+      current_page = agent.get(url)     # リンク取得ぺージの指定
+      product_url = current_page.search('h3 a')     # リンクの取得元を指定
+      product_url.each do |ele|
+         links << ele.get_attribute('href')    # 個別ページをlink配列に代入
+      end
+      # 取得したリンクが相対パスなのでURL追加とメソッドの呼び出し
+      links.each do |link|
+          puts scraping_detail('http://www.gcdental.co.jp' + link)
     end
 
-      links = []      # 個別ページのリンクを保存する配列
+    def self.scraping_detail(links)
       agent = Mechanize.new
       agent.user_agent_alias = 'Windows IE 7'
-      url = 'http://www.gcdental.co.jp/sys/data/category/20/'      # 取得ぺージの指定(認証画面にまず飛ぶ)
-      page = agent.get(url)
-      next_page = page.link_with(:text => 'は　い').click      # 認証画面で「はい」をクリック
+      # 個別ぺージの指定と認証画面で「はい」をクリック
+      page = agent.get(links)
+      next_page = page.link_with(:text => 'は　い').click
+      current_page = agent.get(links)     # リンク取得ぺージの指定
 
-      current_page = agent.get("http://www.gcdental.co.jp/sys/data/category/20/")     # リンク取得ぺージの指定
-      elements = current_page.search('h3 a')     # リンクの取得元を指定
-      elements.each do |ele|
-         links << ele.get_attribute('href')    # 画像取得ページへのリンクを取得
-        end
+      # データの取得
+      image_url = 'http://www.gcdental.co.jp' +current_page.at('dt img').get_attribute('src') if page.at('.dt img')
+      product_url = 'http://www.gcdental.co.jp' +current_page.at('h3 a').get_attribute('href') if page.at('h3 a')
+      product_name = current_page.at('h3 a').inner_text if page.at('h3 a')
+      info = current_page.at('dd h4').inner_text if page.at('dd h4')
 
-      links.each do |link|     # 取得したリンクが相対パスなのでURL追加とメソッドの呼び出し
-          puts scraping_image('http://www.gcdental.co.jp' + link)
-        end
+      # テーブルにデータの保存
+      product = Product.new(product_name: product_name, image_url: image_url, product_url: product_url, info: info)
+      product.save
+    end
 
-  end
 
 end
