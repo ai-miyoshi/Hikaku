bb054d86ed3299ef3b6b385ec15860c95486e282
三吉愛 <ai_miyoshi@miyoshiai-no-MacBook-Pro.local>
2016-08-28 20:49:01 +0900

    edit routes & show_html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @products = Product.new
+    @product = Product.find(params[:id])
   end
 
   def search
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :reviews
   get 'products/index'
-
+  get 'products/:id' => 'products#show'
   get 'reviews/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
