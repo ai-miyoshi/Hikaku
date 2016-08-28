24f5b53d073732fee7ff8358418cf2856aef1ee3
三吉愛 <ai_miyoshi@miyoshiai-no-MacBook-Pro.local>
2016-08-28 18:35:21 +0900

    edit index_view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,13 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.order('id Asc').limit(10)
+  end
+
+  def show
+    @products = Product.new
+  end
+
+  def search
+    @products = []
   end
 end
