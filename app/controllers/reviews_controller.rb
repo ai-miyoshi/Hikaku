cbc4f19d41f0f99425f3b48b205cbfda5d4a3884
三吉愛 <ai_miyoshi@miyoshiai-no-MacBook-Pro.local>
2016-08-29 19:20:06 +0900

    save review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,14 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  def create
+    Review.create(create_params)
+    redirect_to controller: :products, action: :index
+  end
+
+private
+  def create_params
+    params.require(:review).permit(:nickname, :rate, :use_period, :review).merge(product_id: params[:product_id])
+  end
+
 end
