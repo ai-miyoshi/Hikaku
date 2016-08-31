ffc7cbb3315cb2b8104591d1f54111de52c4a639
三吉愛 <ai_miyoshi@miyoshiai-no-MacBook-Pro.local>
2016-08-31 12:42:04 +0900

    add user edit view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :users, only: :show
+  resources :users, only: [:show, :edit]
   resources :products, only: :show do
     resources :reviews, only: [:new, :create]
     collection do
