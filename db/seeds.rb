766d574084d678e8c33f94fefdcd8987476ba566
三吉愛 <ai_miyoshi@miyoshiai-no-MacBook-Pro.local>
2016-09-07 21:18:03 +0900

    edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@ require "csv"
 corporations_csv = CSV.readlines("db/corporations.csv")
 corporations_csv.shift
 corporations_csv.each do |row|
-  Company.create(corporate_name: row[1], c_url: row[2], image_url: row[3])
+  Corporation.create(corporate_name: row[1], c_url: row[2], image_url: row[3])
 end
