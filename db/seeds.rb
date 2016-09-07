125a369e9179793b0dd1319c89ddc9725647786e
三吉愛 <ai_miyoshi@miyoshiai-no-MacBook-Pro.local>
2016-09-07 21:05:24 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
   User.create(email: 'tech-camp@gmail.com', password: '12345678', nickname: 'Mako')
+
+
+require "csv"
+
+corporations_csv = CSV.readlines("db/corporations.csv")
+corporations_csv.shift
+corporations_csv.each do |row|
+  Company.create(corporate_name: row[1], c_url: row[2], : image_url[3])
+end
