2891a97930bf1b7c6dd79a32112dc035748e724d
三吉愛 <ai_miyoshi@miyoshiai-no-MacBook-Pro.local>
2016-09-07 21:15:20 +0900

    edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-  User.create(email: 'tech-camp@gmail.com', password: '12345678', nickname: 'Mako')
+User.create(email: 'tech-camp@gmail.com', password: '12345678', nickname: 'Mako')
 
 
 require "csv"
@@ -14,5 +6,5 @@ require "csv"
 corporations_csv = CSV.readlines("db/corporations.csv")
 corporations_csv.shift
 corporations_csv.each do |row|
-  Company.create(corporate_name: row[1], c_url: row[2], : image_url[3])
+  Company.create(corporate_name: row[1], c_url: row[2], image_url: row[3])
 end
